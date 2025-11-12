3dbcaef0ef82c2536ae2cf5b895ee7321c506ef8
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-11-13 02:59:06 +0600

    Replace AutoImageProcessor with DetrImageProcessorFast for improved image processing

--- img.py
+++ img.py
@@ -1,10 +1,10 @@
-from transformers import AutoImageProcessor
+from transformers import DetrImageProcessorFast
 from torchvision.io import read_image
 
 model_name = "Qwen/Qwen3-VL-8B-Instruct"
 
 
-processor = AutoImageProcessor.from_pretrained(model_name, use_fast=True)
+processor = DetrImageProcessorFast.from_pretrained(model_name, use_fast=True)
 
 images = read_image("./fordcasepage3.png")
 images_processed = processor(images, return_tensors="pt", device="cuda")
