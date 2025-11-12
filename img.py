3112f717de5d0270957d3c056e521c9947673309
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2025-11-13 03:00:44 +0600

    Update image path to absolute URL in img.py for consistent image loading

--- img.py
+++ img.py
@@ -6,7 +6,7 @@ model_name = "Qwen/Qwen3-VL-8B-Instruct"
 
 processor = DetrImageProcessorFast.from_pretrained(model_name)
 
-images = read_image("./fordcasepage3.png")
+images = read_image("file:///workspace/ai-create-embeddings/fordcasepage3.png")
 images_processed = processor(images, return_tensors="pt", device="cuda")
 
 print(images_processed.keys())
