d9813f15d88383ad312e807c0b4cf6ac0f0502c5
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-02-04 08:18:19 +0600

    Refactor query execution by removing filter and entity count retrieval

--- query.py
+++ query.py
@@ -5,13 +5,9 @@ COLLECTION_NAME = "client_demo_embeddings"
 
 client = MilvusClient(uri=MILVUS_URI)
 res = client.query(
-  collection_name=COLLECTION_NAME,
-  filter="parent_id == 'entityasset_AZu-72vdIKT_Qmrri3W9'",
+  collection_name=COLLECTION_NAME
 )
 
 print(res)
-
-total_rows = client.count_entities(collection_name=COLLECTION_NAME)
-print({"total_rows": total_rows})
 
 client.close()
